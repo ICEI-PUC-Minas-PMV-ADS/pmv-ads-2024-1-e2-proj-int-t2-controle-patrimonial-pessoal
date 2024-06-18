13f7ec9d7f115ceb8da515e1fe30842acade6d7f
Rodiney Branta <1485753@sga.pucminas.br>
2024-06-18 10:47:27 -0300

    chore: Update registration form styles and layout
    Implement style enhancements for the registration
    form, including centering the form on the page,
    reducing its size to allow more background
    visibility, and ensuring responsiveness across
    devices

--- src/economus-cpp/economus-cpp/ViewModels/RegisterViewModel.cs
+++ src/economus-cpp/economus-cpp/ViewModels/RegisterViewModel.cs
@@ -5,16 +5,19 @@ namespace economus_cpp.ViewModels
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [Display(Name = "Seu Nome:")]
         public string Name { get; set; }
         [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
         [EmailAddress]
+        [Display(Name = "E-mail:")]
         public string Email { get; set; }
         [Required(ErrorMessage = "O campo Senha é obrigatório.")]
         [DataType(DataType.Password)]
+        [Display(Name = "Senha:")]
         public string Password { get; set; }
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "A senha e a senha de confirmação não coincidem.")]
-        [Display(Name = "Confirmar Senha")]
+        [Display(Name = "Confirmar Senha:")]
         public string ConfirmPassword { get; set; }
     }
 }
