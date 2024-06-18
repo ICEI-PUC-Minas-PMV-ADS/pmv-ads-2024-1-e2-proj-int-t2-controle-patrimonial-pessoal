ae0d4593f821d7ecd828a71d7b36d2dfcf941927
Rodiney Branta <1485753@sga.pucminas.br>
2024-06-18 13:58:30 -0300

    style: Enhance UI spacing and update login functionality Adjust margins for better spacing on the homepage, refine the login view model, and improve the login page layout to match the registration form style.

--- src/economus-cpp/economus-cpp/ViewModels/LoginViewModel.cs
+++ src/economus-cpp/economus-cpp/ViewModels/LoginViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace economus_cpp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace economus_cpp.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        [Display(Name = "Senha")]
         public string Password { get; set; }
+        [Display(Name = "Guardar Acesso")]
         public bool RememberMe { get; set; }
     }
 }
