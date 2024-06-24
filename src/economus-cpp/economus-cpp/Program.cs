da42be8672932c20514b45eb5c886f0d294f2305
Rodiney Branta <1485753@sga.pucminas.br>
2024-06-24 18:19:18 -0300

    feat: Enhance password validation and registration UI - (Program.cs) Update password validation settings to enforce stronger security requirements - (RegisterViewModel.cs) Include password validation error messages - (Register.cshtml) Improve user interface for registration form to display validation errors - (economus-cpp.csproj) Updated project file to reflect dependencies and changes related to registration enhancements

--- src/economus-cpp/economus-cpp/Program.cs
+++ src/economus-cpp/economus-cpp/Program.cs
@@ -11,7 +11,14 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
 
